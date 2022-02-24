fd6cc3ed95ad1f1466894dfc256572c4e624a333
Hardart <kaino@bk.ru>
2022-02-24 11:03:20 +0300

    rename const api to apiVK

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,28 @@
 require('dotenv').config()
 const express = require('express')
 const router = express.Router()
-const url = require('url');
+const url = require('url')
 const fetch = require('node-fetch')
-const api = require('node-vk-bot-api/lib/api')
+const apiVK = require('node-vk-bot-api/lib/api')
 
 router.get('/', async (req, res) => {
-	const {query} = url.parse(req.url, true)
-	if(query.code) {
+	const { query } = url.parse(req.url, true)
+	if (query.code) {
 		const authUrl = `https://oauth.vk.com/access_token?client_id=${process.env.VK_APP_ID}&client_secret=${process.env.VK_APP_TOKEN}&redirect_uri=${process.env.VK_REDIRECT_URL}&code=${query.code}`
 
 		const response = await fetch(authUrl, {
-			method: 'GET'
-		}).then(res => res.json())
+			method: 'GET',
+		}).then((res) => res.json())
 
-		const user = await api('users.get', {
+		const user = await apiVK('users.get', {
 			peer_id: response.user_id,
 			access_token: response.access_token,
-			fields: 'photo_100'
+			fields: 'photo_100',
 		}).then((user) => user.response[0])
-		
+
 		return res.redirect('/')
 	}
-	
+
 	res.render('auth', {
 		title: 'Авторизация',
 	})
