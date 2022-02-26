a8b3286f6a2515a07c0916d5c3367947daa6aac2
Hardart <kaino@bk.ru>
2022-02-26 17:20:39 +0300

    сделал перемещение аватара при выборе

--- public/testScript.js
+++ public/testScript.js
@@ -1,25 +1,35 @@
-let inputElement = document.querySelector('input')
-let slider = document.querySelector('.track')
-let valueElement = document.querySelector('.grabber')
+const inputElement = document.querySelector('input')
+const slider = document.querySelector('.track')
+const valueElement = document.querySelector('.grabber')
+const avatar = document.querySelector('.image-enabled')
+const barFill = document.querySelector('.bar-fill')
+const avatarBorders = document.querySelector('.overlay-avatar')
+
+const avatarWidth = avatar.clientWidth
+const avatarHeight = avatar.clientHeight
 
 valueElement.onmousedown = function (event) {
 	let shiftX = event.clientX - valueElement.getBoundingClientRect().left
 
 	function moveAt(pageX) {
 		let moveLeft = pageX - shiftX - slider.getBoundingClientRect().left
+		// console.log(moveLeft)
 
-		if (moveLeft < 0) {
-			valueElement.style.left = 0 + '%'
-			inputElement.setAttribute('value', 0)
-			return
+		let moveLefInPercent = (moveLeft * 100) / slider.clientWidth
+		if (moveLefInPercent < 0) {
+			moveLefInPercent = moveLeft = 0
 		}
-		if (moveLeft >= slider.clientWidth) {
-			valueElement.style.left = 100 + '%'
-			inputElement.setAttribute('value', 100)
-			return
+		if (moveLefInPercent > 100) {
+			moveLefInPercent = 100
+			moveLeft = slider.clientWidth
 		}
-		const moveLefInPercent = (moveLeft * 100) / slider.clientWidth
+
+		avatar.style.width = avatarWidth + moveLeft
+		avatar.style.minWidth = avatarWidth + moveLeft
+		avatar.style.height = avatarHeight + moveLeft
+
 		valueElement.style.left = moveLefInPercent + '%'
+		barFill.style.width = moveLefInPercent + '%'
 		inputElement.setAttribute('value', Math.round(moveLefInPercent))
 	}
 
@@ -27,16 +37,50 @@ valueElement.onmousedown = function (event) {
 		moveAt(event.clientX)
 	}
 
+	function onMouseUp() {
+		document.removeEventListener('mousemove', onMouseMove)
+		document.removeEventListener('mouseup', onMouseUp)
+	}
+
 	// передвигаем мяч при событии mousemove
 	document.addEventListener('mousemove', onMouseMove)
-
-	document.onmouseup = function () {
-		document.removeEventListener('mousemove', onMouseMove)
-		document.onmouseup = null
-	}
+	document.addEventListener('mouseup', onMouseUp)
 }
 
-// valueElement.ondragstart = function () {
-// 	console.log('drag')
-// 	return false
-// }
+let x = 0
+let y = 0
+
+avatar.onmousedown = function (event) {
+	let shiftX = event.clientX - x
+	let shiftY = event.clientY - y
+
+	function moveAt(pageX, pageY) {
+		avatar.style.transform = `translate3d(${pageX - shiftX}px, ${pageY - shiftY}px, 0px)`
+		x = pageX - shiftX
+		y = pageY - shiftY
+		// console.log(moveH)
+	}
+
+	function onMouseMove(event) {
+		moveAt(event.pageX, event.pageY)
+	}
+
+	function onMouseUp() {
+		document.removeEventListener('mousemove', onMouseMove)
+		document.removeEventListener('mouseup', onMouseUp)
+	}
+
+	// передвигаем мяч при событии mousemove
+	document.addEventListener('mousemove', onMouseMove)
+	document.addEventListener('mouseup', onMouseUp)
+}
+
+avatar.onclick = (e) => {
+	let { x } = getTranslateValues(avatar)
+	console.log(x)
+	console.log('ClientX = ' + e.clientX)
+	console.log('LayerX = ' + e.layerX)
+}
+document.ondragstart = function () {
+	return false
+}
